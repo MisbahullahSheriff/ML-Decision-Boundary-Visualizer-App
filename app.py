48756553a149877eb05c4b3b6233b34b9de0c037
MisbahullahSheriff <misbahullahsheriff@gmail.com>
2024-01-20 12:26:19 +0530

    create page footer

--- app.py
+++ app.py
@@ -29,3 +29,20 @@ st.markdown("""
             
 ### This app will plot the Decision Boundary of a given Machine Learning Classifier for any given dataset
 """)
+
+# page footer
+st.markdown("""
+---
+#### Created By - Mohammed Misbahullah Sheriff
+""")
+
+github_url = "https://github.com/MisbahullahSheriff"
+linkedin_url = "https://www.linkedin.com/in/mohammed-misbahullah-sheriff/"
+
+st.markdown(
+    f'<div style="display: flex;">'
+    f'<a href="{linkedin_url}" style="margin-right: 10px; padding: 8px 20px; background-color: #1e6ed6; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">LinkedIN</a>'
+    f'<a href="{github_url}" style="padding: 8px 20px; background-color: #2a8503; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">GitHub</a>'
+    f'</div>',
+    unsafe_allow_html=True
+)
